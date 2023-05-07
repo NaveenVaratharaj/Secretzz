c8515f96ef5adc0c008e2591bdd727d0770bf4eb
NaveenVaratharaj <naveenvm.cs20@bitsathy.ac.in>
2023-05-07 16:18:45 +0530

    Final Commit with Backend

--- app.js
+++ app.js
@@ -12,39 +12,59 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://127.0.0.1:27017/JournlDB');
+mongoose.connect('mongodb://127.0.0.1:27017/SecretsDB');
 
-let secrets = [];
+const postSchema = {
+  postTitle: String,
+  dateGiven: Date,
+  postBody: String
+};
+
+const Post = mongoose.model("Post", postSchema);
 
 app.get("/", (req,res) => {
   res.render("home.ejs")
 })
 
 app.get("/secrets", (req,res) => {
-  res.render("main.ejs", {secrets : secrets})
+  Post.find({}, function(err, posts){
+   res.render("main", {
+     secrets: posts
+    });
+ })
 })
 
 app.get("/secrets/:secretsBrief", (req,res) =>{
   const reqTitle = _.lowerCase(req.params.secretsBrief);
   console.log(reqTitle)
 
-  secrets.forEach((secret)=>
-  {
-    const storedTitle =  _.lowerCase(secret.postTitle);
-    console.log(storedTitle)
-    const storedDetails = secret.postBody;
+  // Post.forEach((secret)=>
+  // {
+  //   const storedTitle =  _.lowerCase(secret.postTitle);
+  //   // console.log(storedTitle)
+  //   const storedDetails = secret.postBody;
 
-    var secretPost = {
-      storedTitle : secret.postTitle,
-      storedDetails : secret.postBody
-    }
+  //   var secretPost = {
+  //     storedTitle : secret.postTitle,
+  //     storedDetails : secret.postBody
+  //   }
     
-    if(storedTitle === reqTitle)
-    {
-      res.render("secret.ejs", {secrets: secretPost});
-    }
-    else console.log("Not found")
-  })
+  //   if(storedTitle === reqTitle)
+  //   {
+  //     res.render("secret.ejs", {secrets: secretPost});
+  //   }
+  //   else console.log("Not found")
+  // })
+
+  Post.findOne({postTitle: reqTitle}, function(err, post){
+    res.render("secret", { 
+      secret:
+      {
+        storedTitle : post.postTitle,
+        storedDetails : post.postBody
+      }
+    });
+  });
 })
 
 app.get("/compose", (req,res) => {
@@ -59,10 +79,16 @@ app.post("/compose", (req,res) =>{
     postBody : req.body.postBody
   };
 
-  secrets.push(userData);
+  // secrets.push(userData);
+  var post = new Post(userData)
+  post.save();
   res.redirect('/secrets');
 })
 
+app.get("/main", (req,res) =>
+{
+  res.render("main");
+})
 
 app.listen(8000, ()=>{
     console.log("App started at the port 8000");
